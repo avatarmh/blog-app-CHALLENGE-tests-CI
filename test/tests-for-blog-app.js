7de3cb5c9d9ac71b1a67c5b6b02701e34202ac8b
Miriam Heller <miriam.heller@gmail.com>
2018-06-10 03:04:41 -0400

    add normal case test for delete

--- test/tests-for-blog-app.js
+++ test/tests-for-blog-app.js
@@ -144,21 +144,21 @@ describe('BlogPosts', function () {
   //     });
   // });
 
-  //   // test strategy:
-  //   //  1. GET recipes so we can get ID of one
-  //   //  to delete.
-  //   //  2. DELETE an item and ensure we get back a status 204
-    // it('should delete items on DELETE', function() {
-    //   return chai.request(app)
-    //     // first have to get so we have an `id` of item
-    //     // to delete
-    //     .get('/recipes')
-    //     .then(function(res) {
-    //       return chai.request(app)
-    //         .delete(`/recipes/${res.body[0].id}`);
-    //     })
-    //     .then(function(res) {
-    //       expect(res).to.have.status(204);
-    //     });
-    // });
+    // test strategy:
+    //  1. GET recipes so we can get ID of one
+    //  to delete.
+    //  2. DELETE an item and ensure we get back a status 204
+    it('should delete items on DELETE', function() {
+      return chai.request(app)
+        // first have to get so we have an `id` of item
+        // to delete
+        .get('/blog-posts')
+        .then(function(res) {
+          return chai.request(app)
+            .delete(`/blog-posts/${res.body[0].id}`);
+        })
+        .then(function(res) {
+          expect(res).to.have.status(204);
+        });
+    });
 });
