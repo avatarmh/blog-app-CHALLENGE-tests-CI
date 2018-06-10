7df5dc29afd797e60dd529d1fa918be26f16811d
Miriam Heller <miriam.heller@gmail.com>
2018-06-10 11:17:10 -0400

    test for put endpoint

--- test/tests-for-blog-app.js
+++ test/tests-for-blog-app.js
@@ -103,46 +103,49 @@ describe('BlogPosts', function () {
       })
   });
 
-  // //   // test strategy:
-  // //   //  1. initialize some update data (we won't have an `id` yet)
-  // //   //  2. make a GET request so we can get an item to update (because there's no db)
-  // //   //  3. add the `id` to `updateData`
-  // //   //  4. Make a PUT request with `updateData`
-  // //   //  5. Inspect the response object to ensure it
-  // //   //  has right status code and that we get back an updated
-  // //   //  item with the right data in it.
-  // it('should update posts on PUT', function () {
-  //   // we initialize our updateData here and then after the initial
-  //   // request to the app, we update it with an `id` property so
-  //   // we can make a second, PUT call to the app.
-   
-  //   return chai.request(app)
-  //     // first have to get data in memory so we have something to update
-  //     .get('/blog-posts')
-  //     .then(function (res) {
-  //       const updateData = Object.assign(res.body[0], {
-  //         title: 'Theory of Relativity',
-  //         author: 'Albert Einstein'
-  //         });
-  //       console.log(`*** then ${updateData}`);
-  //     // this will return a promise whose value will be the response
-  //       // object, which we can inspect in the next `then` block. Note
-  //       // that we could have used a nested callback here instead of
-  //       // returning a promise and chaining with `then`, but we find
-  //       // this approach cleaner and easier to read and reason about.
-  //       return chai.request(app)
-  //         .put(`/blog-posts/${updateData[0].id}`)
-  //         .send(updateData);
-  //     })
-  //     // prove that the PUT request has right status code
-  //     // and returns updated item
-  //     .then(function (res) {
-  //       expect(res).to.have.status(204);
-  //       expect(res).to.be.json;
-  //       expect(res.body).to.be.a('object');
-  //       expect(res.body).to.deep.equal(updateData);
-  //     });
-  // });
+  //   // test strategy:
+  //   //  1. initialize some update data (we won't have an `id` yet)
+  //   //  2. make a GET request so we can get an item to update (because there's no db)
+  //   //  3. add the `id` to `updateData`
+  //   //  4. Make a PUT request with `updateData`
+  //   //  5. Inspect the response object to ensure it
+  //   //  has right status code and that we get back an updated
+  //   //  item with the right data in it.
+  it('should update blogs on PUT', function () {
+    // we initialize our updateData here and then after the initial
+    // request to the app, we update it with an `id` property so
+    // we can make a second, PUT call to the app.
+    const updateData = {
+      title: 'Theory of Relativity',
+      author: 'Albert Einstein',
+      content: 'foo bar baz',
+      publishDate: '2018-06-10'
+      };
+    console.log('updateData = ', updateData);
+    return chai.request(app)
+      // first have to get data in memory so we have something to update
+      .get('/blog-posts')
+      .then(function (res) {
+        updateData.id = res.body[0].id;
+        console.log(`id = ${updateData.id}`);
+        // this will return a promise whose value will be the response
+        // object, which we can inspect in the next `then` block. Note
+        // that we could have used a nested callback here instead of
+        // returning a promise and chaining with `then`, but we find
+        // this approach cleaner and easier to read and reason about.
+        return chai.request(app)
+          .put(`/blog-posts/${updateData.id}`)
+          .send(updateData);
+      })
+      // prove that the PUT request has right status code
+      // and returns updated item
+      .then(function (res) {
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('object');
+        expect(res.body).to.deep.equal(updateData);
+      });
+  });
 
     // test strategy:
     //  1. GET recipes so we can get ID of one
